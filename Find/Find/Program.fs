e3ee938b098d2980dd1f9bef3fa8561d98c180ad
ususucsus <usarti01@gmail.com>
2021-02-14 13:35:17 +0300

    solution

---
+++ Find/Find/Program.fs
@@ -0,0 +1,11 @@
+﻿let find list value =
+    let rec _find list value position =
+        match list with
+        | [] -> None
+        | head :: _ when head = value -> Some(position)
+        | _ :: tail -> _find tail value (position + 1)
+    
+    _find list value 0
+
+let list = [1; 2; 3; 2; 1]
+printfn "%A" (find list 2)
