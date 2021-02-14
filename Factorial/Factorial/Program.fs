f235bdc3e7fa52f7ae87e43e9698b029510bfbaa
ususucsus <usarti01@gmail.com>
2021-02-14 14:50:25 +0300

    solution

---
+++ Factorial/Factorial/Program.fs
@@ -0,0 +1,16 @@
+﻿let factorial n =
+    let rec _factorial n acc =
+        match n with
+        | 0 -> acc
+        | _ -> _factorial (n - 1) (acc * n)
+    
+    if n < 0 then invalidArg "n" "n should be greater than zero."
+    
+    _factorial n 1
+    
+printfn "%A" (factorial 4)
+printfn "%A" (factorial 3)
+printfn "%A" (factorial 2)
+printfn "%A" (factorial 1)
+printfn "%A" (factorial 0)
+printfn "%A" (factorial -1)
