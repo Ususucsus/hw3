013c1fd76082d2092b009ba01ba7f7de6fb3e1b9
ususucsus <usarti01@gmail.com>
2021-02-14 14:04:54 +0300

    solution

---
+++ Reverse/Reverse/Program.fs
@@ -0,0 +1,10 @@
+﻿let reverse list =
+    let rec _reverse list acc =
+        match list with
+        | [] -> acc
+        | head :: tail -> _reverse tail (head :: acc)
+    
+    _reverse list []
+
+let list = [1..1000000]
+printfn "%A" (reverse list)
